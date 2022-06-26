52f4104a5c8aa8ae98c778c0df1040039f0e3abf
rivalsolmons <76931978+rivalsolmons@users.noreply.github.com>
2022-06-26 11:11:34 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# Real-time-chat-app
+It is a real time chat application built with HTML, CSS, Javascript and Socket.io
---
+++ server.js
@@ -0,0 +1,29 @@
+const { Console } = require('console')
+const express = require('express')
+const app = express()
+const http = require('http').createServer(app)
+
+//client ke data ko listen karega es port par
+const PORT = process.env.PORT || 3000
+http.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`)
+})
+
+app.use(express.static(__dirname + '/public'))
+
+app.get('/',(req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+//socket
+
+const io = require('socket.io')(http)
+
+io.on('connection', (socket) => {
+    console.log('connected...')
+    socket.on('message',(msg) => {
+       socket.broadcast.emit('message',msg)
+    })
+
+
+})
